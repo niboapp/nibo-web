25352a90a42e0770ea7db3a990ba2472b9fa4c9b
Ehbraheem <ehbraheem@gmail.com>
2024-11-11 15:41:15 +0100

    Fix route resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = './';
+const base = process.env.NODE_ENV === 'production' ? '/nibo-web/' : '/';
 
 export default defineConfig({
   plugins: [react()],
