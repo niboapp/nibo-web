ac52510e1a6f57fbed84e87d934a1561b29ada49
Ehbraheem <ehbraheem@gmail.com>
2024-11-11 15:32:31 +0100

    Fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.NODE_ENV === 'production' ? '/nibo-web/' : '/';
+const base = '/';
 
 export default defineConfig({
   plugins: [react()],
