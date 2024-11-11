dbc7057df94300cda234891245c67919a54c7e15
Bolatan Ibrahim <ehbraheem@gmail.com>
2024-11-11 17:35:40 +0100

    CICD: Deploy app to GH pages on push (#3)
    * Fix vite build
    
    * Fix vite build
    
    * Fix vite build
    
    * Fix vite build
    
    * Fix route resolution
    
    * Fix route resolution
    
    * Fix route resolution
    
    * Fix route resolution
    
    * Build artifact settings
    
    * Build artifact settings
    
    * Build artifact settings
    
    * Add domain name
    
    * Change route config to work with custom domain
    
    * Only deploy on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,67 +1,64 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 // Import your main layout and route components
-import Layout from "./components/Layout"; // The layout with the Outlet
-import HomePage from "./pages/HomePage";
-import Categories from "./pages/Categories";
-import Account from "./pages/Account";
-import Help from "./pages/Help";
-import products from "./data/products";
-import { SearchProvider } from "./context/SearchContext";
-import { StoreDetails } from "./pages/StorePage";
-import StoresPage from "./pages/StoresPage";
-import Cart from "./pages/Cart";
-import { CartProvider } from "./context/CartContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Product from "./pages/Product";
-import { ProductDetails } from "./components/ProductDetails";
-import ErrorPage from "./pages/ErrorPage";
+import Layout from './components/Layout'; // The layout with the Outlet
+import HomePage from './pages/HomePage';
+import Categories from './pages/Categories';
+import Account from './pages/Account';
+import Help from './pages/Help';
+import products from './data/products';
+import { SearchProvider } from './context/SearchContext';
+import { StoreDetails } from './pages/StorePage';
+import StoresPage from './pages/StoresPage';
+import Cart from './pages/Cart';
+import { CartProvider } from './context/CartContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Product from './pages/Product';
+import { ProductDetails } from './components/ProductDetails';
+import ErrorPage from './pages/ErrorPage';
 
 // Define routes using createBrowserRouter
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />, // Layout with Outlet for nested routes
     errorElement: <ErrorPage />,
     children: [
-      { path: "", element: <HomePage products={products} /> },
-      { path: "categories", element: <Categories products={products} /> },
-      { path: "account", element: <Account /> },
-      { path: "help", element: <Help /> },
+      { path: '', element: <HomePage products={products} /> },
+      { path: 'categories', element: <Categories products={products} /> },
+      { path: 'account', element: <Account /> },
+      { path: 'help', element: <Help /> },
     ],
   },
-  { path: "cart", element: <Cart /> },
+  { path: 'cart', element: <Cart /> },
   {
-    path: "/product/:id",
+    path: '/product/:id',
     element: <Product />,
   },
   {
-    path: "/productdetail/:id",
+    path: '/productdetail/:id',
     element: <ProductDetails />,
   },
   {
-    path: "/store/:storeName",
+    path: '/store/:storeName',
     element: <StoreDetails />,
   },
-  {
-    path: "stores",
-    element: <StoresPage />,
-  },
+  { path: 'stores', element: <StoresPage /> },
 ]);
 
 // Render the RouterProvider
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-     <SearchProvider>
-      <CartProvider>
-        <RouterProvider router={router} />
-      </CartProvider>
-    </SearchProvider>
+      <SearchProvider>
+        <CartProvider>
+          <RouterProvider router={router} />
+        </CartProvider>
+      </SearchProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+  build: {
+    cssCodeSplit: true,
+    cssMinify: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: 'terser',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
+});
