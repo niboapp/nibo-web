7d786172c3fa246f1fa649a7aed41d7938c976bd
TheOne001 <paul.agbogun@gmail.com>
2025-06-26 11:48:05 +0100

    refactor: change the backend url.

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ import { Toaster } from "sonner";
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_BASE_API_URL
     ? `${import.meta.env.VITE_BASE_API_URL}/graphql`
-    : "https://nibo-api-production.up.railway.app/graphql",
+    : "https://nibo-api.onrender.com/graphql",
 });
 
 const client = new ApolloClient({
